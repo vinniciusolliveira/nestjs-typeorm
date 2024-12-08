381a2ca85cff6e8680c4cc04af374e43beb630b8
Vinicius Oliveira <vinni@vinni>
2024-12-08 04:13:55 -0300

    primeiro commit

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
